522cfa419bb3ac86059722f26c2d82e2c82ef78b
BadLuckk <dariomereu1999@gmail.com>
2024-07-16 12:41:33 +0200

    fakestagram login

--- website/src/App.js
+++ website/src/App.js
@@ -1,16 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Login from "./pages/Login";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         
-        <p>
-          hello maria.
-        </p>
-        
-      </header>
+       < Login />
+
     </div>
   );
 }
